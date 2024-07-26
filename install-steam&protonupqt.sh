3624146e20b26371e14c644d0765a07dd013cf69
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-26 09:55:19 -0600

    Update install-steam&protonupqt.sh

--- install-steam&protonupqt.sh
+++ install-steam&protonupqt.sh
@@ -13,7 +13,7 @@ wget http://media.steampowered.com/client/installer/steam.deb
 sudo dpkg -i steam.deb
 
 # Install any missing dependencies and finish configuring the package
-sudo apt-get install -f -y
+sudo apt install -f -y
 
 # Download ProtonUp-Qt
 wget https://github.com/DavidoTek/ProtonUp-Qt/releases/download/v2.9.2/ProtonUp-Qt-2.9.2-x86_64.AppImage

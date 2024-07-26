ae7c8b9c9eedc4a3cc1b2cae12e5b649d3397d98
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-26 09:31:35 -0600

    Update remove-apps.sh

--- remove-apps.sh
+++ remove-apps.sh
@@ -5,6 +5,11 @@ sudo apt remove --purge -y libreoffice*
 sudo apt clean -y
 sudo apt autoremove -y
 
+# Commands to remove Firefox
+sudo apt remove --purge -y firefox*
+sudo apt clean -y
+sudo apt autoremove -y
+
 # Commands to remove Thunderbird Mail
 sudo apt remove --purge -y thunderbird*
 sudo apt clean -y

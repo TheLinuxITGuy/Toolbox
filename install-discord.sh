4e2ee4c73d5be42137cc6aadb1c03401b698f740
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-26 11:42:56 -0600

    Add files via upload

---
+++ install-discord.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo -e "\033[0;32m====================================="
+echo -e "\033[1;32mThe Linux IT Guy - Linux Mint Scripts"
+echo -e "\033[1;32mInstalling Discord"
+echo -e "\033[0;32m=====================================\033[0m"
+
+# Update the package list
+sudo apt update
+
+# Install flatpak
+sudo apt install -y flatpak
+
+# Add the Flathub repository
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Install Discord
+flatpak install flathub com.discordapp.Discord

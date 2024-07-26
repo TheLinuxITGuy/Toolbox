158d489e450a70ab7a97734e8a9c57d6572ffe42
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-26 10:10:58 -0600

    Update remove-apps.sh

--- remove-apps.sh
+++ remove-apps.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo -e "\033[0;32m====================================="
+echo -e "\033[1;32mThe Linux IT Guy - Linux Mint Scripts"
+echo -e "\033[1;32mRemoving Unwanted Apps"
+echo -e "\033[0;32m=====================================\033[0m"
+
 # Commands to remove Libre Office
 sudo apt remove --purge -y libreoffice*
 sudo apt clean -y

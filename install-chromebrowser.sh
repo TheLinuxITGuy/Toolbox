4eaff8ab15c48353ba8b7b5ad7127c39f9c93ec0
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-26 09:18:50 -0600

    Add files via upload

---
+++ install-chromebrowser.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Navigate to the directory where you want to download the .deb file
+cd ~/Downloads
+
+# Download the latest Google Chrome .deb package
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Install the package
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+
+# Install any missing dependencies and finish configuring the package
+sudo apt-get install -f

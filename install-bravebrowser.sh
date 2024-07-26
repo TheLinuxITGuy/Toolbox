a3625e66f05ac23d75067bca8c39c9c1e0786eb3
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-26 09:29:46 -0600

    Add files via upload

---
+++ install-bravebrowser.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Import the Brave repository's GPG key
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+
+# Add the Brave repository to your system
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+# Update the package list
+sudo apt update
+
+# Install Brave
+sudo apt install -y brave-browser

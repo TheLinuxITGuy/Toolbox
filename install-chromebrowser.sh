f57de2e77a3266a1933d6815ca31228833b9f9e1
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-26 11:13:09 -0600

    Update install-chromebrowser.sh

--- install-chromebrowser.sh
+++ install-chromebrowser.sh
@@ -11,8 +11,13 @@ cd ~/Downloads
 # Update the package list
 sudo apt update
 
-# Download the latest Google Chrome .deb package
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# Check if the .deb package is already downloaded
+if [ ! -f google-chrome-stable_current_amd64.deb ]; then
+    # Download the latest Google Chrome .deb package
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+else
+    echo "Google Chrome .deb package is already downloaded."
+fi
 
 # Install the package
 sudo dpkg -i google-chrome-stable_current_amd64.deb

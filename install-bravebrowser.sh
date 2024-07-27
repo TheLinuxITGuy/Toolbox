2da14c831df20dc64cc06be913ff381b5c033b34
The Linux IT Guy <158848334+TheLinuxITGuy@users.noreply.github.com>
2024-07-26 19:06:01 -0600

    Update install-bravebrowser.sh

--- install-bravebrowser.sh
+++ install-bravebrowser.sh
@@ -5,14 +5,21 @@ echo -e "\033[1;32mThe Linux IT Guy - Linux Mint Scripts"
 echo -e "\033[1;32mInstalling Brave Browser"
 echo -e "\033[0;32m=====================================\033[0m"
 
-# Import the Brave repository's GPG key
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+#!/bin/bash
 
-# Add the Brave repository to your system
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+echo -e "\033[0;32m====================================="
+echo -e "\033[1;32mThe Linux IT Guy - Linux Mint Scripts"
+echo -e "\033[1;32mInstalling Brave Browser"
+echo -e "\033[0;32m=====================================\033[0m"
 
 # Update the package list
 sudo apt update
 
+# Install flatpak
+sudo apt install -y flatpak
+
+# Add the Flathub repository
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 # Install Brave
-sudo apt install -y brave-browser
+flatpak install flathub com.brave.Browser
